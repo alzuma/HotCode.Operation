6195980f7e16b6918bf919612f4a5f6033be993d
Kristaps Vitolins <kristaps.vitolins@gmail.com>
2020-04-10 23:35:52 +0300

    add swagger and endpoint to get command status

---
+++ HotCode.Operation/Controllers/OperationsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using HotCode.Operation.Services;
+using HotCode.Operation.Services.Models;
+using HotCode.System;
+using HotCode.System.Messaging.interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotCode.Operation.Controllers
+{
+    [ApiVersion("1.0")]
+    public class OperationsController : BaseController
+    {
+        private readonly IOperationRepository _operationRepository;
+
+        public OperationsController(IBusPublisher busPublisher, IOperationRepository operationRepository) : base(
+            busPublisher)
+        {
+            _operationRepository = operationRepository;
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OperationModel>> Get(string id)
+            => Single(await _operationRepository.GetAsync(id));
+    }
+}
--- HotCode.Operation/Startup.cs
+++ HotCode.Operation/Startup.cs
@@ -1,10 +1,15 @@
+using System;
+using System.IO;
+using System.Reflection;
 using HotCode.System.Messaging.RedisMq;
 using HotCode.System.Redis;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using ServiceLocator;
 
 namespace HotCode.Operation
@@ -24,10 +29,33 @@ namespace HotCode.Operation
             services.AddControllers();
             services.AddRedisMessaging();
             services.AddRedis();
+            
+            services.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+            });
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1.0", new OpenApiInfo {Title = "Hot code Operation", Version = "v1.0"});
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1.0/swagger.json", "Hot code Operation v1.0");
+                c.RoutePrefix = string.Empty;
+            });
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
